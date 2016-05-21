dab9c2304532e874c4424a153e4cc326dcf6f5a5
Kazuki Nakajima <nkjm@Kazukis-MacBook-Air.local>
2016-05-21 13:31:45 +0900

    first code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* Callback */
 router.post('/callback', function(req, res, next) {
-    console.log(req.body);
+    console.log(req.body.to);
     res.send(null);
 });
 
