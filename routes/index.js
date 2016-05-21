884a4026f5234065f36f660867096361178344fa
Kazuki Nakajima <nkjm@Kazukis-MacBook-Air.local>
2016-05-21 13:24:35 +0900

    first code

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+/* Callback */
+router.get('/callback', function(req, res, next) {
+    console.log(req.body);
+    res.send(null);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
