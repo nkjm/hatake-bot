b92ae35a4706687857e6c01002cb5633519ec1f3
Kazuki Nakajima <nkjm@Kazukis-MacBook-Air.local>
2016-11-02 10:56:23 +0900

    changed from Trial Bot to Message API

--- linebot.js
+++ linebot.js
@@ -1,51 +1,131 @@
 'use strict';
 
-const apiai = require('apiai');
+const APIAI = require('apiai');
 const uuid = require('node-uuid');
 const request = require('request');
+const utility = require('./utility.js');
+const crypto = require('crypto');
 const HatakeDb = require('./hatakeDb.js');
 const hatakeConfig = require('./hatakeConfig.js');
-const utility = require('./utility.js');
+
+const LINE_CHANNEL_ID = process.env.LINE_CHANNEL_ID;
+const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
+const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+const BOT_ADMIN_LINE_MID = process.env.BOT_ADMIN_LINE_MID;
+const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
+const APIAI_LANG = process.env.APIAI_LANG;
 
 module.exports = class LineBot {
 
-    get apiaiService() {
-        return this._apiaiService;
+    static validateSignature(signature, rawBody){
+        // Signature Validation
+        let hash = crypto.createHmac('sha256', LINE_CHANNEL_SECRET).update(rawBody).digest('base64');
+        if (hash != signature) {
+            return false;
+        }
+        return true;
     }
 
-    set apiaiService(value) {
-        this._apiaiService = value;
-    }
-
-    get botConfig() {
-        return this._botConfig;
-    }
-
-    set botConfig(value) {
-        this._botConfig = value;
-    }
-
-    get sessionIds() {
-        return this._sessionIds;
-    }
-
-    set sessionIds(value) {
-        this._sessionIds = value;
-    }
-
-    constructor(botConfig) {
-        this._botConfig = botConfig;
-        var apiaiOptions = {
-            language: botConfig.apiaiLang,
-            requestSource: "line"
-        };
-
+    static processMessage(messageText, translatedMessageText, sourceUserId, replyToken) {
         // apiai sdkのインスタンスを初期化。
-        this._apiaiService = apiai(botConfig.apiaiAccessToken, apiaiOptions);
+        let apiai = APIAI(APIAI_ACCESS_TOKEN, {
+            language: APIAI_LANG,
+            requestSource: "line"
+        });
+        let sessionId = uuid.v1();
+        let apiaiRequest = apiai.textRequest(translatedMessageText,　{sessionId: sessionId});
 
-        this._sessionIds = new Map();
+        // api.aiからのメッセージ受信後のコールバック。
+        apiaiRequest.on('response', (response) => {
+
+            let responseText;
+            let that = this;
+
+            // レスポンスのresultが空の場合。通常は有りえない。
+            if (!utility.isDefined(response.result)) {
+                console.log('Received empty result');
+                responseText = "私、ちょっと具合が悪い見たいです。良いエンジニアに診てもらわないと・・";
+                LineBot.replyMessage(replyToken, responseText);
+                return;
+            }
+
+            // api.aiに該当するactionが設定されていないか、うまく特定できなかった場合。
+            if (!utility.isDefined(response.result.action)) {
+                console.log("Action is not defined on api.ai.");
+                responseText = "う、、すみません、わかりません。";
+                LineBot.replyMessage(replyToken, responseText);
+
+                // Botの管理者にメッセージを送信。
+                LineBot.pushMessage(BOT_ADMIN_LINE_MID, 'た、助けてください。わからないこと聞かれました。「' + message.content.text + '」「' + message.content.translatedText + '」');
+                return;
+            }
+
+            // レスポンスが正しく返され、actionも設定されているので、あとはactionに応じた処理を実装していく。
+            switch (response.result.action) {
+                case "get-latest-moisture":
+                    // 土壌湿度情報をクラウドデータベースに問い合わせ、その値に応じてレスポンスを設定します。
+                    HatakeDb.getLatestMoisture(function(moisture){
+                        responseText = LineBot.getResponseByMoisture(moisture);
+                        console.log("Response: " + responseText);
+                        if (utility.isDefined(responseText)) {
+                            LineBot.replyMessage(replyToken, responseText);
+                        } else {
+                            responseText = "・・・";
+                            LineBot.replyMessage(replyToken, responseText);
+                        }
+                    });
+                    break;
+                case "get-latest-light":
+                    // 仮の実装で固定の値を常に返しています。本来は照度センサーの値を取得してレスポンスを設定するイメージです。
+                    responseText = "いやー、きついですね。帽子かぶりたいくらいです。";
+                    LineBot.replyMessage(replyToken, responseText);
+                    break;
+                case "get-latest-nutrition":
+                    // 仮の実装で固定の値を常に返しています。本来は土壌養分の値を取得してレスポンスを設定するイメージです。
+                    responseText = "肥え、プリーズ。";
+                    LineBot.replyMessage(replyToken, responseText);
+                    break;
+                case "get-latest-soil":
+                    // 土壌湿度とほぼ同じ処理です。
+                    HatakeDb.getLatestMoisture(function(moisture){
+                        responseText = "水分については、" + LineBot.getResponseByMoisture(moisture);
+                        console.log("Response: " + responseText);
+                        if (utility.isDefined(responseText)) {
+                            LineBot.replyMessage(replyToken, responseText);
+                        } else {
+                            responseText = "・・・";
+                            LineBot.replyMessage(replyToken, responseText);
+                        }
+                    });
+                    break;
+                default:
+                    // api.ai側にはアクションが設定されているものの、このBotプログラム上に処理が実装されていなかった場合。
+                    console.log("Action: '" + response.result.action + "' is not implemented.");
+
+                    // もしapi.ai側でspeechが設定されていた場合、それをそのままレスポンスメッセージに使用します。
+                    if (utility.isDefined(response.result.fulfillment) && utility.isDefined(response.result.fulfillment.speech)){
+                        responseText = response.result.fulfillment.speech;
+                        LineBot.replyMessage(replyToken, responseText);
+                    } else {
+                        responseText = "う、、すみません、わかりません。";
+                        LineBot.replyMessage(replyToken, responseText);
+
+                        // Botの管理者にメッセージを送信。
+                        LineBot.pushMessage(BOT_ADMIN_LINE_MID, 'た、助けてください。わからないこと聞かれました。「' + message.content.text + '」「' + message.content.translatedText + '」');
+                    }
+
+                    break;
+            }
+        });
+
+        // api.aiでエラー発生時のコールバック。
+        apiaiRequest.on('error', (error) => console.error(error));
+
+        // api.aiインスタンスの設定は完了。api.aiへのコールアウト開始。
+        apiaiRequest.end();
     }
 
+    /*
     // メッセージをapi.aiで処理する。
     processMessage(message, res) {
         // messageに必要な情報（送信元Midとメッセージ）があるか確認
@@ -154,8 +234,10 @@ module.exports = class LineBot {
         // api.aiインスタンスの設定は完了。api.aiへのコールアウト開始。
         apiaiRequest.end();
     }
+    */
 
     // メッセージを送ってきたユーザーにLINEで返信する。
+    /*
     postLineMessage(to, text) {
         request.post("https://trialbot-api.line.me/v1/events", {
             headers: {
@@ -187,6 +269,7 @@ module.exports = class LineBot {
             console.log('Send LINE message succeeded');
         });
     }
+    */
 
     // あらかじめ設定された土壌湿度閾値に応じて返信メッセージ本文を返す。
     static getResponseByMoisture(moisture){
@@ -202,4 +285,114 @@ module.exports = class LineBot {
         }
         return response;
     }
+
+    static replyMessage(replyToken, message){
+        return new Promise(function(resolve, reject){
+            let headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            };
+            let body = {
+                replyToken: replyToken,
+                messages: [message]
+            }
+            let url = 'https://api.line.me/v2/bot/message/reply';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            }, function (error, response, body) {
+                (error) ? reject(error) : resolve();
+            });
+        });
+    }
+
+    static pushMessage(to, message){
+        return new Promise(function(resolve, reject){
+            let headers = {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+            };
+            let body = {
+                to: to,
+                messages: [message]
+            }
+            let url = 'https://api.line.me/v2/bot/message/push';
+            request({
+                url: url,
+                method: 'POST',
+                headers: headers,
+                body: body,
+                json: true
+            }, function (error, response, body) {
+                (error) ? reject(error) : resolve();
+            });
+        });
+    }
+
+    /*
+    /  New Bot api
+    */
+    /*
+    const LINE_CHANNEL_ID = process.env.LINE_CHANNEL_ID;
+    const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
+    const LINE_CHANNEL_ACCESS_TOKEN = process.env.LINE_CHANNEL_ACCESS_TOKEN;
+    const LINE_MID = process.env.LINE_MID;
+    const BOT_ADMIN_LINE_MID = process.env.BOT_ADMIN_LINE_MID;
+    const crypto = require('crypto');
+    const request = require('request');
+    const Promise = require('bluebird');
+
+    module.exports = class LineBot {
+
+        static pushMessage(to, message){
+            return new Promise(function(resolve, reject){
+                let headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+                };
+                let body = {
+                    to: to,
+                    messages: [message]
+                }
+                let url = 'https://api.line.me/v2/bot/message/push';
+                request({
+                    url: url,
+                    method: 'POST',
+                    headers: headers,
+                    body: body,
+                    json: true
+                }, function (error, response, body) {
+                    (error) ? reject(error) : resolve();
+                });
+            });
+        }
+
+        static replyMessage(replyToken, message){
+            return new Promise(function(resolve, reject){
+                let headers = {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + LINE_CHANNEL_ACCESS_TOKEN
+                };
+                let body = {
+                    replyToken: replyToken,
+                    messages: [message]
+                }
+                let url = 'https://api.line.me/v2/bot/message/reply';
+                request({
+                    url: url,
+                    method: 'POST',
+                    headers: headers,
+                    body: body,
+                    json: true
+                }, function (error, response, body) {
+                    (error) ? reject(error) : resolve();
+                });
+            });
+        }
+
+    };
+    */
 };
--- routes/webhook.js
+++ routes/webhook.js
@@ -5,33 +5,56 @@ const router = express.Router();
 
 const crypto = require('crypto');
 const LineBot = require('../linebot');
-const LineBotConfig = require('../linebotconfig');
 const googleTranslate = require('google-translate')(process.env.GOOGLE_API_KEY);
-const APIAI_ACCESS_TOKEN = process.env.APIAI_ACCESS_TOKEN;
-const APIAI_LANG = process.env.APIAI_LANG;
-const LINE_CHANNEL_ID = process.env.LINE_CHANNEL_ID;
-const LINE_CHANNEL_SECRET = process.env.LINE_CHANNEL_SECRET;
-const LINE_MID = process.env.LINE_MID;
-const BOT_ADMIN_LINE_MID = process.env.BOT_ADMIN_LINE_MID;
-
-
-// -----------------------------------------------------------------------------
-// ボット関連インスタンス初期化
-const botConfig = new LineBotConfig(
-    APIAI_ACCESS_TOKEN,
-    APIAI_LANG,
-    LINE_CHANNEL_ID,
-    LINE_CHANNEL_SECRET,
-    LINE_MID,
-    BOT_ADMIN_LINE_MID
-);
-const bot = new LineBot(botConfig);
-// -----------------------------------------------------------------------------
-
 
 // -----------------------------------------------------------------------------
 // ルーティング設定
 
+// LINEからのコールバックがこのURLにPOSTされます。
+router.post('/', (req, res) => {
+
+    // Signature Validation
+    if (!LineBot.validateSignature(req.get('X-Line-Signature'), req.rawBody)){
+        return res.status(401).send('Signature validation failed.');
+    }
+    console.log('Signature validation succeeded.');
+
+    try {
+
+        for (let botEvent of req.body.events){
+            // 受信したメッセージをGoogle Translate APIで英語に翻訳。
+            if (botEvent.type == "message"){
+                googleTranslate.translate(botEvent.message.text, 'en', function(err, translation) {
+                    if (err){
+                        console.log(err);
+                        return;
+                    }
+                    console.log("Translation: " + translation.translatedText);
+
+                    // 英語翻訳したメッセージをセット。
+                    botEvent.message.translatedText = translation.translatedText;
+
+                    // 英語翻訳したメッセージを含むitemオブジェクトをapi.aiにかける。
+                    LineBot.processMessage(
+                        botEvent.message.text,
+                        translation.translatedText,
+                        botEvent.source.userId,
+                        botEvent.replyToken
+                    );
+                });
+            }
+        }
+
+    } catch (err) {
+        res.status(400).send('Error while processing ' + err.message);
+        return;
+    }
+
+    res.status(200).end();
+});
+// -----------------------------------------------------------------------------
+
+/*
 // LINEからのコールバックがこのURLにPOSTされます。
 router.post('/', (req, res) => {
 
@@ -77,6 +100,6 @@ router.post('/', (req, res) => {
     res.status(200).end();
 });
 // -----------------------------------------------------------------------------
-
+*/
 
 module.exports = router;
